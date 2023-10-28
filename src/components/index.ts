b731b5bfac1eed8be2f145564814f65c5de6aaf2
Josias Buday Dias <josiasbudaydeveloper@gmail.com>
2023-10-28 05:32:00 -0300

    Making the basic structure of the app.

---
+++ src/components/index.ts
@@ -0,0 +1,29 @@
+import Billing from "./billing";
+import Business from "./Business";
+import Button from "./Button";
+import CardDeal from "./CarDeal";
+import Clients from "./Clients";
+import CTA from "./CTA";
+import FeedbackCard from "./FeedbackCard";
+import Footer from "./Footer";
+import GetStarted from "./GetStarted";
+import Hero from "./hero";
+import NavBar from "./navbar";
+import Stats from "./Stats";
+import Testimonials from "./Testimonials";
+
+export {
+  Billing,
+  Business,
+  Button,
+  CardDeal,
+  Clients,
+  CTA,
+  FeedbackCard,
+  Hero,
+  Footer,
+  GetStarted,
+  NavBar,
+  Stats,
+  Testimonials
+}
