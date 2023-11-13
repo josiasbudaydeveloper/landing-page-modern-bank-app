1ca5ccad9c889c6669dc948aa77d74a354eaa398
Josias Buday Dias <josiasbudaydeveloper@gmail.com>
2023-11-13 20:41:01 -0300

    Business Section

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,5 +1,11 @@
-export default function Button() {
+export default function Button({styles}: {styles: string}) {
   return (
-    <div>Button</div>
+    <button type="button" className={`
+      py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px] text-primary outline-none
+      rounded-[10px]
+      ${styles}
+    `}>
+      Get Started
+    </button>
   )
 }
