0857b389dc0f82fb1d1cbdf4cdab99a0f3b24098
Josias Buday Dias <josiasbudaydeveloper@gmail.com>
2023-11-18 02:14:42 -0300

    Adding the CTA Section

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,4 +1,4 @@
-export default function Button({styles}: {styles: string}) {
+export default function Button({ styles = "" }: {styles?: string}) {
   return (
     <button type="button" className={`
       py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px] text-primary outline-none
